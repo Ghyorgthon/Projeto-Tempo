2583a4494393364d7e2aa93cabee1acea8681f42
Ghyorgthon André <136536865+Ghyorgthon@users.noreply.github.com>
2023-09-28 19:02:12 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,33 +7,27 @@ const weatherIcon = document.querySelector('.weather-icon')
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city +'&appid=${apiKey}')
+    var data = await response.json()
+    console.log(data)
 
-    if (response.status == 404){
-        document.querySelector('.erro').style.display='block'
-    } 
-    else{
-        var data = await response.json()
-        console.log(data)
+    document.querySelector('.city').innerHTML = data.name
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°C"
+    document.querySelector('.humidity').innerHTML = data.main.humidity + "%"
+    document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h'
 
-        document.querySelector('.city').innerHTML = data.name
-        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°C"
-        document.querySelector('.humidity').innerHTML = data.main.humidity + "%"
-        document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h'
-
-        if(data.weather[0].main == 'Clouds'){
-            weatherIcon.src = 'images/clouds.png'
-        } else if(data.weather[0].main == 'Rain'){
-            weatherIcon.src = 'images/rain.png'
-        }else if (data.weather[0].main == 'Clear'){
-            weatherIcon.src = 'images/clear.png'
-        }else if (data.weather[0].main == 'Drizzle'){
-            weatherIcon.src = 'images/drizzle.png'
-        }else if (data.weather[0].main == 'Mist'){
-            weatherIcon.src = 'images/mist.png'
+    if(data.weather[0].main == 'Clouds'){
+        weatherIcon.src = 'images/clouds.png'
+    } else if(data.weather[0].main == 'Rain'){
+        weatherIcon.src = 'images/rain.png'
+    }else if (data.weather[0].main == 'Clear'){
+        weatherIcon.src = 'images/clear.png'
+    }else if (data.weather[0].main == 'Drizzle'){
+        weatherIcon.src = 'images/drizzle.png'
+    }else if (data.weather[0].main == 'Mist'){
+        weatherIcon.src = 'images/mist.png'
         }
 
         clima.innerHTML = data.weather[0].description
-        document.querySelector('.erro').style.display='none'
         document.querySelector('.search input').value = ''
 
     }
